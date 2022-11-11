1601f969d04d9c9ffcf6e9887774998cb7d492be
ionchik <69267618+ionchik@users.noreply.github.com>
2022-11-11 23:21:40 +0300

    PZ_7 was done.

--- PZ_7/PZ_7_2.py
+++ PZ_7/PZ_7_2.py
@@ -3,8 +3,9 @@
 # строке, заменив в нем все предыдущие вхождения его последней буквы на символ
 # «.» (точка). Например, слово «МИНИМУМ» надо преобразовать в «.ИНИ.УМ».
 # Количество пробелов между словами не изменять.
+import re
 
-income_words = input("Введите строку: ").split()
-pattern = (lambda word: word[:-1].replace(word[-1], ".") + word[-1])
-answer = " ".join(list(map(pattern, income_words)))
+income_words = re.findall(r'\w*', input("Введите строку: "))
+pattern = (lambda word: word[:-1].replace(word[-1], ".") + word[-1] if word else " ")
+answer = "".join(list(map(pattern, income_words)))
 print(f"Ответ - {answer}")
